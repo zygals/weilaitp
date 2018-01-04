ee1c885d17428adba01832db61afd6884d73bb8d
zygals <2799024884@qq.com>
2018-01-04 14:03:53 +0800

    index

--- application/back/validate/SeoSetValidate.php
+++ application/back/validate/SeoSetValidate.php
@@ -7,24 +7,15 @@ class SeoSetValidate extends Validate{
 	protected $rule = [
 		'nav_id'  =>  'require|in:1,2,3,4,5,6,7,8,9',
 		'title' =>  'require',
-		'keywords' =>  'require|max:255',
-		//'description' =>  'require|max:255',
-
+		'keywords' =>  'require',
 	];
 	protected $message  =   [
 		'nav_id.require' => '导航必须',
 		'title.require'   => 'seo标题必须',
-		'title.max'   => 'seo标题字数超限',
 		'keywords.require'   => 'seo关键词必须',
-		'keywords.max'   => 'seo关键词字数超限',
         'description.require'   => 'seo描述必须',
-		'description.max'   => 'seo描述字数超限',
-
 
 	];
-	protected $scene = [
-		//'goodnew'  =>  ['name','cate_id'],
 
-	];
 
 }
